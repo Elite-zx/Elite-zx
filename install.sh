3fff5f3d1c47a9f6c8b0016aa181088d849850cf
EliteZX <elite2022zx@gmail.com>
2023-08-01 22:07:43 +0800

    shell script to install

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ sudo apt update
 sudo apt upgrade -y
 
 # basic developer tool, such as gcc,g++,make
-sudo apt install build-essential
+sudo apt install build-essential -y
 
 #----------------------------------------zsh---------------------------------------- 
-sudo apt install zsh
+sudo apt install zsh -y
 
 # zsh Powerlevel10k theme
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 # colorls
-sudo apt install ruby-full
-sudo gem install colorls
+sudo apt install ruby-full -y
+sudo gem install colorls  -y
 
 # bat
-sudo apt install bat
+sudo apt install bat -y
 
 # duf
 apt install duf
@@ -34,7 +34,7 @@ sudo apt install vim-gtk3 -y
 sudo apt install fcitx5 \
 fcitx5-chinese-addons \
 fcitx5-frontend-gtk4 fcitx5-frontend-gtk3 fcitx5-frontend-gtk2 \
-fcitx5-frontend-qt5
+fcitx5-frontend-qt5 -y
 
 # install dict
 wget https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.4/zhwiki-20220416.dict  
@@ -43,7 +43,7 @@ mv zhwiki-20220416.dict ~/.local/share/fcitx5/pinyin/dictionaries/
 
 #-------------------------------------------shortcut tool----------------------------------------
 # install flameshot
-sudo apt install flameshot
+sudo apt install flameshot -y
 
 #--------------------------------------------use dpkg tools------------------------------------
 # dpkg --get-selections > selections.txt
@@ -60,8 +60,8 @@ git clone https://github.com/vinceliuice/WhiteSur-icon-theme
 cd  WhiteSur-icon-theme
 ./install.sh
 
-sudo apt install chrome-gnome-shell
-sudo apt install gnome-shell-extensions
+sudo apt install chrome-gnome-shell -y
+sudo apt install gnome-shell-extensions -y
 
 
 
